03e17589660cdd58379adce41e1c4306d430a56d
ednaleiden <ednadupont@hotmail.com>
2024-07-28 18:25:20 -0500

    jso y jstl

---
+++ src/main/java/com/proyectoJspyJSTL/JspYJstlApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoJspyJSTL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspYJstlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspYJstlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoJspyJSTL/JspYJstlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoJspyJSTL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspYJstlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
